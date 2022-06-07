68b2d406445b75eafa0ccfe0aa431b72a60db840
alecec <alessio.cecchi@infohospital.it>
2022-06-07 10:13:02 +0200

    IOS PushModalAsync bug

---
+++ Xaminals/Views/Page1.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Xaminals.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPushModal_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new Page2());
+        }
+
+        private void btnHideAnimals_Clicked(object sender, EventArgs e)
+        {
+            AppShell appShell = Shell.Current as AppShell;
+            appShell.SetAnimalsVisibility(false);
+        }
+
+        private void btnShowAnimals_Clicked(object sender, EventArgs e)
+        {
+            AppShell appShell = Shell.Current as AppShell;
+            appShell.SetAnimalsVisibility(true);
+        }
+    }
+}
